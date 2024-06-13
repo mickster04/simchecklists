4e8f0ace627e48173e1279216d0766d49d92ec59
JulianoFlores <julianoflores@outlook.com>
2024-06-13 17:48:12 -0300

    v1.0.3
    Version 1.0.3 (2024/06/13)
    - Fix: Issue #13 - Added auto-scroll only if the clicked item is
    positioned after 65% of the screen height.
    - Fix: Airbus A320neo Checklist (changed Fuel Pumps=ON position to work
    with a320neov2 and should be Toliss correct, if anyone can confirm).
    - Tweak: Mininum swipe to uncheck is now user's screen width divided by
    two (window.screen / 2).
    - Tweak: Renamed the c172_530.txt checklist to Cessna 172 Skyhawk
    (G530), since it is not exclusive to X-Plane 11.
    - Added: Airbus A320neo (Asobo V1) Checklist.
    - Added: Cessna 152 Checklist.
    - Added: Beechcraft Bonanza G36 Checklist.

--- assets/checks.js
+++ assets/checks.js
@@ -2,12 +2,15 @@ var touchstartX = 0;
 var touchendX = 0;
 var itemsList = document.getElementsByClassName('items');
 var itemHighlight = itemsList[0];
+var width = screen.width / 2;
 itemHighlight.classList.add('highlight');
 
 function cross() {
 	this.classList.add('items-done');
 	this.querySelector(".state").classList.add('state-done');
 	this.querySelector(".strike").style.opacity = "1";
+		
+	if(this.getBoundingClientRect().top > (screen.height *0.65)) { window.scroll({top: (this.getBoundingClientRect().top -10)  + window.scrollY, behavior: 'smooth'}); }
 	
 	for(i =0; i < itemsList.length; i++) { 
 		itemsList[i].classList.remove('highlight');
@@ -31,7 +34,8 @@ for (var i = 0; i < itemsList.length; i++) {
 	itemsList[i].addEventListener('touchend', function(event) { 
 		touchendX = event.changedTouches[0].screenX;
 		if(touchstartX > touchendX) {
-			if( (touchstartX - touchendX) > 80 ) { 
+			console.log(touchstartX - touchendX);
+			if( (touchstartX - touchendX) > width) { 
 				this.querySelector(".state").classList.remove('state-done');
 				this.querySelector(".strike").style.opacity = "0";
 				for(i =0; i < itemsList.length; i++) { itemsList[i].classList.remove('highlight'); }
--- inc/_config.php
+++ inc/_config.php
@@ -4,7 +4,7 @@ session_cache_limiter('private_no_expire');
 session_start();
 if(isset($_POST['theme'])) { $_SESSION['theme'] = substr($_POST['theme'],6); }
 $app_title = "SimChecklists";
-$app_version = "1.0.1";
+$app_version = "1.0.3";
 require_once('inc/_class.checklist.php');
 require_once('inc/_class.theme.php');
 $theme = new theme();
