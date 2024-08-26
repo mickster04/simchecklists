be46e8d335c08123d1ddb82725a211eb0ff0d27f
jflores82 <tibone@gmail.com>
2024-08-26 10:57:25 -0300

    v1.1.9
    Version 1.1.9 (2024/08/26)
    - Added: Secondary and Terciary cronometers.
    - Added: Boeing 747-800 checklist by tibonev.
    - Added: Boeing 727 checklist by Lightdasher360.
    - Added: TBM900 checklist by Lightdasher360.
    - Updated: C337 checklist by Lightdasher360.
    - Updated: Boeing 737-800 checklist by tibonev.

--- assets/chrono.js
+++ assets/chrono.js
@@ -1,6 +1,9 @@
 chrono_open_buttons = document.querySelectorAll('.chrono_open');
 chrono_open_buttons.forEach(function(btn) { btn.addEventListener('click', chrono_open, false); });
 document.getElementById('chrono_close').addEventListener('click', chrono_close, false);
+
+
+// Chronometer 1 //
 document.getElementById('chrono_start').addEventListener('click', chrono_start, false);
 document.getElementById('chrono_stop').addEventListener('click', chrono_stop, false);
 document.getElementById('chrono_reset').addEventListener('click', chrono_reset, false);
@@ -58,4 +61,103 @@ function chrono_update_display() {
 	if(chrono_seg < 10) { chrono_seg_display = "0" + chrono_seg; }
 	if(chrono_min < 10) { chrono_min_display = "0" + chrono_min; }
 	chrono_display.innerHTML = chrono_min_display + ":" + chrono_seg_display;
+}
+
+
+// Chronometer 2 //
+document.getElementById('chrono_start2').addEventListener('click', chrono_start2, false);
+document.getElementById('chrono_stop2').addEventListener('click', chrono_stop2, false);
+document.getElementById('chrono_reset2').addEventListener('click', chrono_reset2, false);
+chrono_display2 = document.getElementById('chrono_display2');
+let timer2;
+var chrono_min2 = 0;
+var chrono_seg2 = 0;
+var chrono_run2 = 0;
+
+function chrono_start2() { 
+	if(chrono_run2 == 0) { 
+		timer2 = window.setInterval(function(){
+			chrono_increase2();
+	  	}, 1000);
+		chrono_run2 = 1;
+	}
+}
+
+function chrono_increase2() { 
+	chrono_seg2 = chrono_seg2 + 1; 
+	if(chrono_seg2 == 60) { 
+		chrono_seg2 = 0;
+		chrono_min2 = chrono_min2 + 1;
+	}
+	chrono_update_display2();
+}
+
+function chrono_stop2() { 
+	clearInterval(timer2);
+	chrono_run2 = 0;
+}
+
+function chrono_reset2() { 
+	if(chrono_run2 == 0) { 
+		chrono_min2 = 0;
+		chrono_seg2 = 0;
+		chrono_update_display2();
+	}
+}
+
+function chrono_update_display2() { 
+	chrono_seg_display2 = chrono_seg2;
+	chrono_min_display2 = chrono_min2;
+	if(chrono_seg2 < 10) { chrono_seg_display2 = "0" + chrono_seg2; }
+	if(chrono_min2 < 10) { chrono_min_display2 = "0" + chrono_min2; }
+	chrono_display2.innerHTML = chrono_min_display2 + ":" + chrono_seg_display2;
+}
+
+// Chronometer 3 //
+document.getElementById('chrono_start3').addEventListener('click', chrono_start3, false);
+document.getElementById('chrono_stop3').addEventListener('click', chrono_stop3, false);
+document.getElementById('chrono_reset3').addEventListener('click', chrono_reset3, false);
+chrono_display3 = document.getElementById('chrono_display3');
+let timer3;
+var chrono_min3 = 0;
+var chrono_seg3 = 0;
+var chrono_run3 = 0;
+
+function chrono_start3() { 
+	if(chrono_run3 == 0) { 
+		timer3 = window.setInterval(function(){
+			chrono_increase3();
+	  	}, 1000);
+		chrono_run3 = 1;
+	}
+}
+
+function chrono_increase3() { 
+	chrono_seg3 = chrono_seg3 + 1; 
+	if(chrono_seg3 == 60) { 
+		chrono_seg3 = 0;
+		chrono_min3 = chrono_min3 + 1;
+	}
+	chrono_update_display3();
+}
+
+function chrono_stop3() { 
+	clearInterval(timer3);
+	chrono_run3 = 0;
+}
+
+function chrono_reset3() { 
+	if(chrono_run3 == 0) { 
+		chrono_min3 = 0;
+		chrono_seg3 = 0;
+		chrono_update_display3();
+	}
+}
+
+function chrono_update_display3() { 
+	chrono_seg_display3 = chrono_seg3;
+	chrono_min_display3 = chrono_min3;
+	if(chrono_seg3 < 10) { chrono_seg_display3 = "0" + chrono_seg3; }
+	if(chrono_min3 < 10) { chrono_min_display3 = "0" + chrono_min3; }
+	chrono_display3.innerHTML = chrono_min_display3 + ":" + chrono_seg_display3;
 }
--- chrono.php
+++ chrono.php
@@ -8,5 +8,26 @@
 			<button id="chrono_stop">Stop</button>
 			<button id="chrono_reset">Reset</button>
 		</div>
+		
+		<br><br>
+		
+		<div class="chrono_display"><span id="chrono_display2">00:00</span></div>
+		<br>
+		<div class="chrono_display spread_items">
+			<button id="chrono_start2">Start</button>
+			<button id="chrono_stop2">Stop</button>
+			<button id="chrono_reset2">Reset</button>
+		</div>
+		
+		
+		<br><br>
+		
+		<div class="chrono_display"><span id="chrono_display3">00:00</span></div>
+		<br>
+		<div class="chrono_display spread_items">
+			<button id="chrono_start3">Start</button>
+			<button id="chrono_stop3">Stop</button>
+			<button id="chrono_reset3">Reset</button>
+		</div>
 	</div>
 </div>
